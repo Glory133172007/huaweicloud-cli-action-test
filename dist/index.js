a1e9bcede4efbd7862415c839794b7830f6c393f
LiXinyu <936239864@qq.com>
2022-07-02 15:03:43 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -222,7 +222,7 @@ exports.installKooCLIOnMacos = installKooCLIOnMacos;
 function installKooCLIOnLinux() {
     return __awaiter(this, void 0, void 0, function* () {
         core.info('current system is Linux.');
-        yield tools.execCommand('curl -sSL https://hwcloudcli.obs.cn-north-1.myhuaweicloud.com/cli/latest/hcloud_install.sh -o ./hcloud_install.sh && bash ./hcloud_install.sh -y');
+        yield tools.execCommand(`curl -sSL https://hwcloudcli.obs.cn-north-1.myhuaweicloud.com/cli/latest/hcloud_install.sh -o ./hcloud_install.sh && bash ./hcloud_install.sh -y`);
     });
 }
 exports.installKooCLIOnLinux = installKooCLIOnLinux;
